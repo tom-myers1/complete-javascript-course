d6472f4f7bff615e932ee7fed7b519af7e4654c3
Tom Myers <tom.myers310@gmail.com>
2018-07-10 23:01:20 +0100

    section 2 complete

---
+++ complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -0,0 +1,70 @@
+/*
+ * variables and data types
+ */
+
+/*
+var first = 'tommy';
+
+console.log(first);
+
+
+var last = 'myers';
+var age = 36;
+var isOld = true;
+var job;
+
+console.log(isOld);
+console.log(age);
+console.log(job);
+
+job = 'learner of things';
+console.log(job);
+*/
+
+/*
+ * Variable mutation
+ */
+/*
+var firstName = 'tom';
+var lastName = 'myers';
+var age = 36;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried, sex;
+job = 'platform engineer';
+isMarried = false;
+sex = 'male';
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. married = ' + isMarried + '. sex = ' + sex);
+
+age = 'younger';
+alert(firstName + ' is a ' + age + ' ' + job + '. married = ' + isMarried + '. sex = ' + sex);
+
+var doing = prompt('what are you doing?');
+
+console.log(doing);
+
+
+*/
+
+
+
+
+var now, yearTom, fullAge;
+now = 2018;
+yearTom = 36;
+fullAge = 18;
+
+var isFullAge = now - yearTom >= fullAge;
+console.log(isFullAge);
+
+x = y = 2-36;
+console.log(x, y);
+
+x += 10;
+y *= 4;
+console.log(x, y);
+
+// increment
+x ++; // rather than x += 1;
