46a08e3da33636ae7e608881c6660785c2e637d7
Tom Myers <tom.myers310@gmail.com>
2018-07-12 20:34:38 +0100

    end test - did in function rather than writing same equation twice - all good

--- complete-javascript-course-master/2-JS-basics/starter/script.js
+++ complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -49,7 +49,7 @@ console.log(doing);
 */
 
 
-
+/*
 
 var now, yearTom, fullAge;
 now = 2018;
@@ -68,3 +68,47 @@ console.log(x, y);
 
 // increment
 x ++; // rather than x += 1;
+
+
+*/
+
+
+
+
+
+
+
+/*****************************
+* CODING CHALLENGE 1
+*/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
+*/
+
+var markHeight, markWeight, markBmi, johnHeight, johnWeight, johnBmi, markHeaviest;
+
+markHeight = 1.3; //m
+markWeight = 84; //kg
+
+johnHeight = 1.2; //m
+johnWeight = 88; //kg
+
+function calBmi(height, weight){
+  console.log('height = ' + height + ' and weight = ' + weight);
+  var bmi = weight / (height * height);
+  console.log('bmi = ' + bmi);
+  return bmi;
+}
+
+markBmi = calBmi(markHeight, markWeight);
+johnBmi = calBmi(johnHeight, johnWeight);
+
+console.log('marks bim = ' + markBmi + ' and johns bmi = ' + johnBmi);
+markHeaviest = markBmi > johnBmi;
+console.log('is mark heaviest? ' + markHeaviest);
